5d023a8bf997d564c0402c1f217c542ae0d13e38
Ivan Morozov <ivan.morozov@onlyoffice.com>
2023-03-10 14:10:26 +0300

    add simple flask site

--- setup.sh
+++ setup.sh
@@ -5,10 +5,10 @@ sudo apt-get update
 sudo apt-get install python3-pip -y
 
 # Install required Python packages
-pip3 install python-telegram-bot
+pip3 install pytelegrambotapi
 
 # add next here
-# pip3 install ...
+pip3 install Flask
 
 echo "All dependencies installed successfully!"
 
