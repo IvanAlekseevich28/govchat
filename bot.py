b0efd1b4d474bfda88081594d39b8dcca7209f02
sasha9012 <65350753+sasha9012@users.noreply.github.com>
2023-03-29 11:37:01 +0300

    SomeComment

--- bot.py
+++ bot.py
@@ -1,51 +1,59 @@
 import telebot
-
-with open('./bottoken.private', 'r') as f:
+from telebot import types
+from node import Node
+with open('bottoken.txt', 'r') as f:
     token = f.read().strip()
 
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Добро пожаловать в госчатбот!")
     main_menu(message)
 
 def main_menu(message):
     markup = telebot.types.ReplyKeyboardMarkup(row_width=1)
-    option_a = telebot.types.KeyboardButton('Нижний Новгород')
-    option_b = telebot.types.KeyboardButton('Нижегородская область')
-    markup.add(option_a, option_b)
-    bot.reply_to(message, "Вы живете в Нижнем Новгороде или в Нижегородской области?", reply_markup=markup)
+    btn1 = telebot.types.KeyboardButton("Начать")
+    markup.add(btn1)  
+    bot.send_message(message.from_user.id, "Добро пожаловать", reply_markup=markup)
 
-@bot.message_handler(func=lambda message: message.text == 'Нижний Новгород')
-def option_nn(message):
+@bot.message_handler(content_types=['text'])
+def talking(message):
+    global Dialog
+    if message.text == "назад":
+       Dialog= Node(["Вы живете в Нижнем Новгороде или в Нижегородской области?","Нижний Новгород"],Node(["Вы знаете в какое ведомство нужно обратиться с Вашей проблемой?","да","нет"],Node(["Выберите ведомство","1","2"],Node(["выбрали1","назад"]),Node(["выбрали2","назад"])),    Node(["Опишите Вашу проблему.","назад"],Node(["назад"]))))
+    Data_len=len(Dialog.data)
+    for i in range(Data_len-1):
+     if message.text == Dialog.data[i+1]:
+        if i==0: 
+            Dialog=Dialog.one 
+            break
+        if i==1: 
+            Dialog=Dialog.two 
+            break
+        if i==2: 
+            Dialog=Dialog.three 
+            break
+        if i==3: 
+            Dialog=Dialog.four 
+            break
     markup = telebot.types.ReplyKeyboardMarkup(row_width=1)
-    option_nn1 = telebot.types.KeyboardButton('да')
-    option_nn2 = telebot.types.KeyboardButton('нет')
-    markup.add(option_nn1, option_nn2)
-    bot.reply_to(message, "Вы знаете в какую организацию нужно обратиться с проблемой/запросом?", reply_markup=markup)
+    option=create_button(Dialog.data[1:])
+    for i in range(len(option)):
+       markup.add(option[i])        
+    bot.reply_to(message, Dialog.data[0], reply_markup=markup)
+    print(Dialog.data[0])     
 
-@bot.message_handler(func=lambda message: message.text == 'нет')
-def option_nn2(message):
-    bot.reply_to(message, "Опишите Вашу проблему.")
+Dialog= Node(["Вы живете в Нижнем Новгороде или в Нижегородской области?","Нижний Новгород"],Node(["Вы знаете в какое ведомство нужно обратиться с Вашей проблемой?","да","нет"],Node(["Выберите ведомство","1","2"],Node(["выбрали1","назад"]),Node(["выбрали2","назад"])),    Node(["Опишите Вашу проблему.","назад"],Node(["назад"]))))
 
-@bot.message_handler(func=lambda message: message.text == 'У нас нет врачей в поликлинике 167, не могу записаться к терапевту.  Сама поликлиника как год закрыта на ремонт.')
-def option_nnNo(message):
-    markup = telebot.types.ReplyKeyboardMarkup(row_width=1)
-    option_nnno1 = telebot.types.KeyboardButton('Нет')
-    option_nnno2 = telebot.types.KeyboardButton('Да, жду ответа')
-    option_nnno3 = telebot.types.KeyboardButton('Да, мне не помогли')
-    option_nnno4 = telebot.types.KeyboardButton('Да, меня не приняли')
-    markup.add(option_nnno1, option_nnno2, option_nnno3, option_nnno4)
-    bot.reply_to(message, "Вы обращались в администрацию поликлиники 167 с данной проблемой?", reply_markup=markup)
+def create_button(buttons):
+    Options=[]
+    for i in range(len(buttons)):
+        Option=telebot.types.KeyboardButton(buttons[i])
+        Options.append(Option)
+    return Options
 
-@bot.message_handler(func=lambda message: message.text == 'Да, мне не помогли')
-def option_nnno3(message):
-    bot.reply_to(message, "Ваше заявление нужно отправить в министерство здравоохранения Нижегородской области – https://zdrav-nnov.ru/. Перефразируйте ваше заявление под шаблон (скачать) и отправите с кодом 4567 в чат. Ответ придет Вам в течении 3 дней. Посмотреть его можно будет в разделе «уведомления», личный аккаунт, сайт «Госуслуги». ")
 
-@bot.message_handler(func=lambda message: True)
-def unknown(message):
-    bot.reply_to(message, "Простите, не смог обработать ваш текущий запрос.")
+
 
 bot.polling()
 
---
+++ node.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self,data,one=None,two=None,three=None,four=None):
+        self.one = one
+        self.two = two
+        self.three = three
+        self.four = four
+        self.data = data
+        
+    def print(tree):
+        return tree.data[0]
